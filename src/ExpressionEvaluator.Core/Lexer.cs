9010326d733a5750683ba0be46212ecfdd617092
Robert Sundström <robert.sundstrom@outlook.com>
2016-09-05 22:04:00 +0200

    Formatted code.

--- src/ExpressionEvaluator.Core/Lexer.cs
+++ src/ExpressionEvaluator.Core/Lexer.cs
@@ -4,137 +4,137 @@ using System.Text;
 
 namespace ExpressionEvaluator
 {
-	/// <summary>
-	/// Lexer.
-	/// </summary>
-	public class Lexer
-	{
-		private LexerState? state;
+    /// <summary>
+    /// Lexer.
+    /// </summary>
+    public class Lexer
+    {
+        private LexerState? state;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="T:ExpressionEvaluator.Lexer"/> class.
-		/// </summary>
-		/// <param name="textReader">Text reader.</param>
-		public Lexer(TextReader textReader)
-		{
-			TextReader = textReader;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ExpressionEvaluator.Lexer"/> class.
+        /// </summary>
+        /// <param name="textReader">Text reader.</param>
+        public Lexer(TextReader textReader)
+        {
+            TextReader = textReader;
 
             Line = 1;
             Column = 1;
-		}
+        }
 
-		/// <summary>
-		/// Gets the text reader.
-		/// </summary>
-		/// <value>The text reader.</value>
-		public TextReader TextReader
-		{
-			get;
-		}
+        /// <summary>
+        /// Gets the text reader.
+        /// </summary>
+        /// <value>The text reader.</value>
+        public TextReader TextReader
+        {
+            get;
+        }
 
-		/// <summary>
-		/// Peeks a token.
-		/// </summary>
-		/// <returns>The token.</returns>
-		public TokenInfo PeekToken()
-		{
-			if (state != null)
-			{
+        /// <summary>
+        /// Peeks a token.
+        /// </summary>
+        /// <returns>The token.</returns>
+        public TokenInfo PeekToken()
+        {
+            if (state != null)
+            {
                 var s = (LexerState)state;
-				var peekedToken = s.Token;
+                var peekedToken = s.Token;
                 Line = s.Line;
                 Column = s.Column;
-				return peekedToken;
-			}
-			else 
-			{
-				var peekToken = ReadTokenCore();
-				state = new LexerState(peekToken, Line, Column);
-				return peekToken;
-			}
-		}
+                return peekedToken;
+            }
+            else
+            {
+                var peekToken = ReadTokenCore();
+                state = new LexerState(peekToken, Line, Column);
+                return peekToken;
+            }
+        }
 
-		/// <summary>
-		/// Reads a token.
-		/// </summary>
-		/// <returns>The token.</returns>
-		public TokenInfo ReadToken()
-		{
-			if (state != null)
-			{
-				var token = state.Value.Token;
-				state = null;
-				return token;
-			}
-			return ReadTokenCore();
-		}
+        /// <summary>
+        /// Reads a token.
+        /// </summary>
+        /// <returns>The token.</returns>
+        public TokenInfo ReadToken()
+        {
+            if (state != null)
+            {
+                var token = state.Value.Token;
+                state = null;
+                return token;
+            }
+            return ReadTokenCore();
+        }
 
-		/// <summary>
-		/// Gets a value indicating whether this <see cref="T:ExpressionEvaluator.Lexer"/> has reached EOF.
-		/// </summary>
-		/// <value><c>true</c> if is EOF; otherwise, <c>false</c>.</value>
-		public bool IsEof
-		{
-			get
-			{
-				return PeekToken().Kind == TokenKind.EndOfFile;
-			}
-		}
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:ExpressionEvaluator.Lexer"/> has reached EOF.
+        /// </summary>
+        /// <value><c>true</c> if is EOF; otherwise, <c>false</c>.</value>
+        public bool IsEof
+        {
+            get
+            {
+                return PeekToken().Kind == TokenKind.EndOfFile;
+            }
+        }
 
-		/// <summary>
-		/// Internal method for reading a token.
-		/// </summary>
-		/// <returns>The token core.</returns>
-		internal TokenInfo ReadTokenCore()
-		{
-			while (!IsEofCore)
-			{
+        /// <summary>
+        /// Internal method for reading a token.
+        /// </summary>
+        /// <returns>The token core.</returns>
+        internal TokenInfo ReadTokenCore()
+        {
+            while (!IsEofCore)
+            {
                 int line = Line;
                 int column = Column;
 
-				var c = PeekChar();
+                var c = PeekChar();
 
-				if (char.IsDigit(c))
-				{
-					var stringBuilder = new StringBuilder();
-					do
-					{
-						ReadChar();
+                if (char.IsDigit(c))
+                {
+                    var stringBuilder = new StringBuilder();
+                    do
+                    {
+                        ReadChar();
 
-						stringBuilder.Append(c);
+                        stringBuilder.Append(c);
 
-						c = PeekChar();
-					} while (char.IsDigit(c));
+                        c = PeekChar();
+                    } while (char.IsDigit(c));
 
-					return new TokenInfo(TokenKind.Number, line, column, stringBuilder.Length, stringBuilder.ToString());
-				}
-				else 
-				{
-					ReadChar();
+                    return new TokenInfo(TokenKind.Number, line, column, stringBuilder.Length, stringBuilder.ToString());
+                }
+                else
+                {
+                    ReadChar();
 
-					switch (c) 
-					{
-						case '+':
-							return new TokenInfo(TokenKind.Plus, line, column, 1);
+                    switch (c)
+                    {
+                        case '+':
+                            return new TokenInfo(TokenKind.Plus, line, column, 1);
 
-						case '-':
-							return new TokenInfo(TokenKind.Minus, line, column, 1);
+                        case '-':
+                            return new TokenInfo(TokenKind.Minus, line, column, 1);
 
-						case '*':
-							return new TokenInfo(TokenKind.Star, line, column, 1);
+                        case '*':
+                            return new TokenInfo(TokenKind.Star, line, column, 1);
 
-						case '/':
-							return new TokenInfo(TokenKind.Slash, line, column, 1);
+                        case '/':
+                            return new TokenInfo(TokenKind.Slash, line, column, 1);
 
-						case '(':
-							return new TokenInfo(TokenKind.OpenParen, line, column, 1);
+                        case '(':
+                            return new TokenInfo(TokenKind.OpenParen, line, column, 1);
 
-						case ')':
-							return new TokenInfo(TokenKind.CloseParen, line, column, 1);
+                        case ')':
+                            return new TokenInfo(TokenKind.CloseParen, line, column, 1);
 
-						case ' ':
-							//return new TokenInfo(TokenKind.Whitespace, 1);
-							break;
+                        case ' ':
+                            //return new TokenInfo(TokenKind.Whitespace, 1);
+                            break;
 
                         case '\r':
                             break;
@@ -145,53 +145,53 @@ namespace ExpressionEvaluator
                             break;
 
                         default:
-						    return new TokenInfo(TokenKind.Invalid, line, column, 1, c.ToString());
-					}
-				}
-			}
+                            return new TokenInfo(TokenKind.Invalid, line, column, 1, c.ToString());
+                    }
+                }
+            }
 
-			return new TokenInfo(TokenKind.EndOfFile, Line, Column, 0);
-		}
+            return new TokenInfo(TokenKind.EndOfFile, Line, Column, 0);
+        }
 
-		private bool IsEofCore
-		{
-			get
-			{
-				return TextReader.Peek() == -1;
-			}
-		}
+        private bool IsEofCore
+        {
+            get
+            {
+                return TextReader.Peek() == -1;
+            }
+        }
 
         private int Line { get; set; }
         private int Column { get; set; }
 
         private char ReadChar()
-		{
+        {
             Column++;
-			return (char)TextReader.Read();
-		}
+            return (char)TextReader.Read();
+        }
 
-		private char PeekChar()
-		{
-			return (char)TextReader.Peek();
-		}
+        private char PeekChar()
+        {
+            return (char)TextReader.Peek();
+        }
 
-		/// <summary>
-		/// Encapsulates a lookahead state for the Lexer..
-		/// </summary>
-		struct LexerState
-		{
-			public LexerState(TokenInfo token, int line, int column)
-			{
-				Token = token;
+        /// <summary>
+        /// Encapsulates a lookahead state for the Lexer..
+        /// </summary>
+        struct LexerState
+        {
+            public LexerState(TokenInfo token, int line, int column)
+            {
+                Token = token;
                 Line = line;
                 Column = column;
-			}
+            }
 
-			public TokenInfo Token { get;}
+            public TokenInfo Token { get; }
 
             public int Line { get; }
 
             public int Column { get; }
         }
-	}
+    }
 }
--- src/ExpressionEvaluator.Core/Parser.cs
+++ src/ExpressionEvaluator.Core/Parser.cs
@@ -2,27 +2,27 @@
 using System;
 namespace ExpressionEvaluator
 {
-	/// <summary>
-	/// Parser.
-	/// </summary>
-	public class Parser
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="T:ExpressionEvaluator.Parser"/> class.
-		/// </summary>
-		/// <param name="lexer">Lexer.</param>
-		public Parser(Lexer lexer)
-		{
-			Lexer = lexer;
+    /// <summary>
+    /// Parser.
+    /// </summary>
+    public class Parser
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ExpressionEvaluator.Parser"/> class.
+        /// </summary>
+        /// <param name="lexer">Lexer.</param>
+        public Parser(Lexer lexer)
+        {
+            Lexer = lexer;
 
             Diagnostics = new DiagnosticsBag();
-		}
+        }
 
-		/// <summary>
-		/// Gets the lexer.
-		/// </summary>
-		/// <value>The lexer.</value>
-		public Lexer Lexer { get; }
+        /// <summary>
+        /// Gets the lexer.
+        /// </summary>
+        /// <value>The lexer.</value>
+        public Lexer Lexer { get; }
 
         /// <summary>
         /// Gets the diagnostics bag.
@@ -35,109 +35,110 @@ namespace ExpressionEvaluator
         /// </summary>
         /// <returns>An expression.</returns>
         public Expression ParseExpression()
-		{
-			return ParseExpressionCore(0);
-		}
+        {
+            return ParseExpressionCore(0);
+        }
 
-		/// <summary>
-		/// Parses an expression. (Internal)
-		/// </summary>
-		/// <returns>An expression.</returns>
-		/// <param name="prec">The current level of precendence.</param>
-		private Expression ParseExpressionCore(int prec)
-		{
-			BinaryOperation operation;
-			int precedence;
+        /// <summary>
+        /// Parses an expression. (Internal)
+        /// </summary>
+        /// <returns>An expression.</returns>
+        /// <param name="prec">The current level of precendence.</param>
+        private Expression ParseExpressionCore(int prec)
+        {
+            BinaryOperation operation;
+            int precedence;
 
-			var expr = ParsePrimaryExpression();
+            var expr = ParsePrimaryExpression();
 
-			while (true) 
-			{
-				var operatorCandidate = Lexer.PeekToken();
+            while (true)
+            {
+                var operatorCandidate = Lexer.PeekToken();
 
-				if (TryResolveOperation(operatorCandidate, out operation, out precedence))
-				{
-					Lexer.ReadToken();
+                if (TryResolveOperation(operatorCandidate, out operation, out precedence))
+                {
+                    Lexer.ReadToken();
 
-					if (precedence >= prec)
-					{
-						var right = ParseExpressionCore(prec + 1);
+                    if (precedence >= prec)
+                    {
+                        var right = ParseExpressionCore(prec + 1);
 
-						expr = new BinaryExpression(operation, expr, right);
-					}
-					else
-					{
-						return expr;
-					}
-				}
-				else 
-				{
-					return expr;
-				}
-			}
-		}
+                        expr = new BinaryExpression(operation, expr, right);
+                    }
+                    else
+                    {
+                        return expr;
+                    }
+                }
+                else
+                {
+                    return expr;
+                }
+            }
+        }
 
-		/// <summary>
-		/// Tries to resolve an operation from a specified candidate token.
-		/// </summary>
-		/// <returns><c>true</c>, if the token is an operation, <c>false</c> otherwise.</returns>
-		/// <param name="candidateToken">The candidate token for operation.</param>
-		/// <param name="operation">The operation corresponding to the candidate token.</param>
-		/// <param name="precedence">The operator precedence for the resolved operation.</param>
-		private bool TryResolveOperation(TokenInfo candidateToken, out BinaryOperation operation, out int precedence)
-		{
-			switch (candidateToken.Kind) 
-			{
-				case TokenKind.Star:
-					operation = BinaryOperation.Multiply;
-					precedence = 2;
-					break;
+        /// <summary>
+        /// Tries to resolve an operation from a specified candidate token.
+        /// </summary>
+        /// <returns><c>true</c>, if the token is an operation, <c>false</c> otherwise.</returns>
+        /// <param name="candidateToken">The candidate token for operation.</param>
+        /// <param name="operation">The operation corresponding to the candidate token.</param>
+        /// <param name="precedence">The operator precedence for the resolved operation.</param>
+        private bool TryResolveOperation(TokenInfo candidateToken, out BinaryOperation operation, out int precedence)
+        {
+            switch (candidateToken.Kind)
+            {
+                case TokenKind.Star:
+                    operation = BinaryOperation.Multiply;
+                    precedence = 2;
+                    break;
 
-				case TokenKind.Slash:
-					operation = BinaryOperation.Divide;
-					precedence = 2;
-					break;
+                case TokenKind.Slash:
+                    operation = BinaryOperation.Divide;
+                    precedence = 2;
+                    break;
 
-				case TokenKind.Plus:
-					operation = BinaryOperation.Add;
-					precedence = 1;
-					break;
+                case TokenKind.Plus:
+                    operation = BinaryOperation.Add;
+                    precedence = 1;
+                    break;
 
-				case TokenKind.Minus:
-					operation = BinaryOperation.Subtract;
-					precedence = 1;
-					break;
-					
-				default:
-					operation = BinaryOperation.None;
-					precedence = -1;
-					return false;
-			}
+                case TokenKind.Minus:
+                    operation = BinaryOperation.Subtract;
+                    precedence = 1;
+                    break;
 
-			return true;
-		}
+                default:
+                    operation = BinaryOperation.None;
+                    precedence = -1;
+                    return false;
+            }
 
-		/// <summary>
-		/// Parses a primary expression.
-		/// </summary>
-		/// <returns>An expression.</returns>
-		private Expression ParsePrimaryExpression()
-		{
-			Expression expr = null;
+            return true;
+        }
 
-			var token = Lexer.ReadToken();
+        /// <summary>
+        /// Parses a primary expression.
+        /// </summary>
+        /// <returns>An expression.</returns>
+        private Expression ParsePrimaryExpression()
+        {
+            Expression expr = null;
 
-			switch (token.Kind) {
-				case TokenKind.OpenParen:
-					expr = ParseExpression();
-					token = Lexer.ReadToken();
-					if(token.Kind != TokenKind.CloseParen)
+            var token = Lexer.ReadToken();
+
+            switch (token.Kind)
+            {
+                case TokenKind.OpenParen:
+                    expr = ParseExpression();
+                    token = Lexer.ReadToken();
+                    if (token.Kind != TokenKind.CloseParen)
                         Diagnostics.AddError(string.Format(Strings.Error_ExpectedToken, ')'), token.GetSpan());
                     break;
 
-				case TokenKind.Number:
-					expr = new NumberExpression(int.Parse(token.Value));
-					break;
+                case TokenKind.Number:
+                    expr = new NumberExpression(int.Parse(token.Value));
+                    break;
 
                 case TokenKind.EndOfFile:
                     Diagnostics.AddError(Strings.Error_UnexpectedEndOfFile, token.GetSpan());
@@ -146,9 +147,9 @@ namespace ExpressionEvaluator
                 default:
                     Diagnostics.AddError(Strings.Error_UnexpectedToken, token.GetSpan());
                     break;
-			}
+            }
 
-			return expr;
-		}
-	}
+            return expr;
+        }
+    }
 }
